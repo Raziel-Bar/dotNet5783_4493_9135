804674a57ff41d58097e3834aff0c275956653e2
Raziel-Bar <rbarchic@g.jct.ac.il>
2022-10-23 20:53:49 +0300

    stage0

---
+++ Stage0/Program4493.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Stage0
+
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4493();
+            Console.ReadKey();
+        }
+        
+        static partial void Welcome9135();
+        private static void Welcome4493()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+
+}
