6d8b8d61896d2b1b962c5b2f2fb7181b189efd5e
Eliezer E. Segall <Esegall@g.jct.ac.il>
2022-10-25 20:06:49 +0300

    second student's welcome func

---
+++ Stage0/Program9135.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial internal class Program
+    {
+        static partial void Welcome9135()
+        {
+            Console.WriteLine("I'm also here! and I'm kinda hungry...");
+        }
+    }
+}
